8e568d4525f79a364b2b3c04871d7faab1679dda
Yen lo Wang <ivanbumaschny@gmail.com>
2020-10-21 20:55:04 -0300

    Auto stash before merge of "master" and "origin/master"

--- ProyectoFinal.m
+++ ProyectoFinal.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 im=imread('001source.jpg');
 im=imresize(im,0.25);
 
@@ -73,3 +74,50 @@ if(round(thisBB(2)+thisBB(4)+1)<=size(im_fil,1)&&round(thisBB(3)+thisBB(1)+1)<=s
    
 end
 end
+=======
+im=imread('001source.jpg');
+im=imresize(im,0.25);
+
+%% Analizo los tres canales
+%{
+subplot(131)
+imshow(im(:,:,1))
+subplot(132)
+imshow(im(:,:,2))
+subplot(133)
+imshow(im(:,:,3))
+%}
+im=im(:,:,2); %Me quedo con el canal G
+
+%% Umbralizaci?n
+U=graythresh(im);
+im_bin=imbinarize(im,U);
+%im_bin=imcomplement(im_bin);
+
+subplot(121)
+imshow(im)
+subplot(122)
+imshow(im_bin)
+
+%% Filtrado morfol?gico
+SE=strel('disk',4);
+
+%{
+im_fil=imerode(im_fil,SE);
+im_fil=imerode(im_fil,SE);
+im_fil=imdilate(im_bin,SE);
+im_fil=imdilate(im_bin,SE);
+%}
+
+
+
+SE=strel('disk',2);
+im_fil=imopen(im_bin,SE);
+%im_fil=imclose(im_fil,SE);
+
+figure
+subplot(121)
+imshow(im_bin)
+subplot(122)
+imshow(im_fil)
+>>>>>>> Stashed changes
